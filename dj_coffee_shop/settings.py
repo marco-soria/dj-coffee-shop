544ab0e8fe3556d2f232b2c5bb07edf85e4b1d69
marco-soria <marco.soriaa7@gmail.com>
2024-09-07 18:38:56 -0500

    added orders and users

--- dj_coffee_shop/settings.py
+++ dj_coffee_shop/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    "crispy_forms",
+    "crispy_tailwind",
+    'products',
+    'orders',
+    'users',
 ]
 
 MIDDLEWARE = [
